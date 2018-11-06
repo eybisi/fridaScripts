8b6248c3d98bdcd0e62939a04c8ef56c6f96d8aa
Ahmet Bilal Can <eybisii@gmail.com>
2018-11-06 10:25:02 +0300

    Update enumerateLoadedClasses.js

--- enumerateLoadedClasses.js
+++ enumerateLoadedClasses.js
@@ -1,35 +1,22 @@
 
 setImmediate(function() {
-
 	Java.perform(function () {
 			Java.enumerateLoadedClasses({
 				onMatch: function(className) {
-
-					if(className.indexOf("ctf.stm")>=0){
+					if(className.indexOf("example.packagename")>=0){
 						console.log(className)
-
 					}
-
 				},
 				onComplete: function(){
 					console.log("Done");
 				}
 			});
 	});
-
-	
-
 			Module.enumerateImports("base.odex", {
 				onMatch: function(className) {
-
 				if(className.name.indexOf("stm")>=0){
 						console.log(className.name)
-
 					}
-						//console.log(className.name)
-
-					
-
 				},
 				onComplete: function(){
 					console.log("Done");
