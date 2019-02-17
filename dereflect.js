3a6a4e82d347d1fac55c0760103f3af03204970c
Ahmet Bilal Can <ahmetbilalcann@gmail.com>
2019-02-18 00:17:15 +0300

    Update dereflect.js

--- dereflect.js
+++ dereflect.js
@@ -1,24 +1,54 @@
 Java.perform(function() {
 
-    var internalClasses = ["android.", "org."]; 
     var classDef = Java.use('java.lang.Class');
     var classLoaderDef = Java.use('java.lang.ClassLoader');
-    var forName = classDef.forName.overload('java.lang.String', 'boolean', 'java.lang.ClassLoader');
     var loadClass = classLoaderDef.loadClass.overload('java.lang.String', 'boolean');
-    var getMethod = classDef.getMethod.overload('java.lang.String', '[Ljava.lang.Object;');
     var reflect = Java.use('java.lang.reflect.Method')
-    //var invoke = reflect.invoke.overload('java.lang.Object', '[Ljava.lang.Object;')
-    //java.lang.reflect.Method.invoke
-
+    var member = Java.use('java.lang.reflect.Member')
+    var dalvik = Java.use("dalvik.system.DexFile")
+    var dalvik2 = Java.use("dalvik.system.DexClassLoader")
+    var dalvik3 = Java.use("dalvik.system.PathClassLoader")
+    var dalvik4 = Java.use("dalvik.system.InMemoryDexClassLoader")
     var f = Java.use("java.io.File")
     var url = Java.use("java.net.URL")
-    
+    var obj = Java.use("java.lang.Object")
+
+    obj.getClass.implementation = function(){
+        o = this.getClass()
+        return this.getClass()
+    }
+    reflect.hashCode.implementation = function (){
+        console.log(this.hashCode())
+        return this.hashCode()
+    }
+    member.getName.implementation = function(){
+        console.log('Getname -> ' + this.getName())
+        return this.getName()
+    }
+    classDef.getMethods.implementation = function(){
+        o = this.getMethods()
+        return this.getMethods()
+    }
+    reflect.invoke.implementatition = function(a,b){
+        console.log("invoke catched -> " + a)
+        this.invoke(a,b)
+    }
     f.$init.overload("java.net.URI").implementation = function(a){
         console.log("URI called")
         f.$init(a)
-
     }
-
+    f.delete.implementation = function(a){
+        console.log("[+] Delete catched =>" +this.getAbsolutePath())
+        return true
+    }
+    dalvik.loadDex.implementation = function(a,b,c){
+        console.log("[+] loadDex Catched -> " + a)
+        return dalvik.loadDex(a,b,c)
+    }
+    dalvik2.$init.implementation = function (a,b,c,d) {
+        console.log("[+] DexClassLoader Catched -> " + a)
+        this.$init(a,b,c,d)
+    }
     forName.implementation = function(class_name, flag, class_loader) {
         var isGood = true;
         for (var i = 0; i < internalClasses.length; i++) {
@@ -29,14 +59,8 @@ Java.perform(function() {
         if (isGood) {
             console.log("Reflection => forName => " + class_name);
         }
-
         return forName.call(this, class_name, flag, class_loader);
     }
-    classDef.getMethod.overload('java.lang.String', '[Ljava.lang.Object;').implementation = function (a,b,c) {
-        console.log("[+] getMethod catched -> " + a)
-        return this.getMethod(a,b)
-    }  
-
     loadClass.implementation = function(class_name, resolve) {
         var isGood = true;
         for (var i = 0; i < internalClasses.length; i++) {
@@ -50,6 +74,3 @@ Java.perform(function() {
         return loadClass.call(this, class_name, resolve);
     }
 });
-
-
-    
